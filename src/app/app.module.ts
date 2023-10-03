bfb736ebf4576cb235bf4499772c1479782d6c72
Remy Arthur <r.arthur@outlook.fr>
2023-10-03 09:21:32 +0200

    Ajout du header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { HeaderComponent } from './components/header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
@@ -7,7 +7,16 @@ import { Observable } from 'rxjs';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   
+  test$:Observable<string>;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void{
+    const url = "http://localhost:3000/api/accomodations";
+    this.test$ = this.http.get<string>(url);
+    // Permet d'afficher une trace de log sur la page web
+    // this.http.get<string>(url).subscribe(url=>console.log(url));
+  }
 }
